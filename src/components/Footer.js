f5b9f3e8078072d60c8fbd1b5fe8215165ba4b8f
dayofmolp <molpofdays@gmail.com>
2024-11-21 13:38:42 +0200

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>With love, always ❤️</p>
+    </footer>
+  );
+};
+
+export default Footer;
