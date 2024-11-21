a4e536b6cf2be3fe9eaa9bc748e3fc8db6138554
dayofmolp <molpofdays@gmail.com>
2024-11-21 13:38:22 +0200

    Create LovePoster.js

---
+++ src/components/LovePoster.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./LovePoster.css";
+
+const LovePoster = () => {
+  return (
+    <div id="poster" className="love-poster">
+      <h2>Love Notes</h2>
+      <p>
+        "You are my sun, my moon, and all my stars. Here's to more magical moments together!"
+      </p>
+    </div>
+  );
+};
+
+export default LovePoster;
